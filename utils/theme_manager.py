b58ffdaf754a02260ae1e4565026caa72c643208
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-11-04 18:01:42 +0100

    Add ThemeManager

---
+++ utils/theme_manager.py
@@ -0,0 +1,19 @@
+import dearpygui.dearpygui as dpg
+import dearpygui_ext.themes as themes
+
+class ThemeManager:
+    def __init__(self):
+        self.light_theme = themes.create_theme_imgui_light()
+        self.dark_theme = themes.create_theme_imgui_dark()
+
+    def set_theme(self, sender, data):
+        if dpg.get_value(sender) == "Light":
+            dpg.bind_theme(self.light_theme)
+        else:
+            dpg.bind_theme(self.dark_theme)
+
+    def create_theme_selector(self):
+        with dpg.window(label="Theme Selector", width=200, height=100, pos=(10, 10)):
+            dpg.add_combo(
+                ["Light", "Dark"], label="Theme", callback=self.set_theme, default_value="Dark"
+            )
