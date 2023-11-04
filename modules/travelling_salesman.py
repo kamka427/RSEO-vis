052a1244bff3b8ed7537f3bde761043d04e71370
Kálmán Neszlényi <73647069+kamka427@users.noreply.github.com>
2023-11-04 17:13:48 +0100

    Add travelling salesman algorithm

---
+++ modules/travelling_salesman.py
@@ -0,0 +1,46 @@
+from sys import maxsize
+from itertools import permutations
+
+from objects.waypoint import Waypoint
+from objects.mission import Mission
+
+
+"""
+This function returns the less energy consuming path for the given waypoints from greedy set cover function.
+"""
+
+
+def travellingSalesmanProblem(
+    selected_waypoints: list[Waypoint],
+) -> Mission:
+    # Add the depo to the list of selected waypoints
+    selected_waypoints.insert(0, Waypoint("depo", 0, 0, set(), 0))
+
+    # Initialize the list of vertices
+    vertex = []
+    s = 0  # starts from the depo
+    for i in range(len(selected_waypoints)):
+        if i != s:
+            vertex.append(i)
+
+    min_path_weight = maxsize
+    min_path = None  # Initialize the min_path as None
+
+    next_permutation = permutations(vertex)
+    for i in next_permutation:
+        current_path_weight = 0
+        path = [selected_waypoints[s]]  # Initialize the path with the source vertex
+        k = s
+        for j in i:
+            current_path_weight += selected_waypoints[k].flying_cost
+            path.append(selected_waypoints[j])
+            k = j
+        current_path_weight += selected_waypoints[k].flying_cost
+
+        if current_path_weight < min_path_weight:
+            min_path_weight = current_path_weight
+            min_path = path
+
+    min_path.append(selected_waypoints[s])
+
+    return Mission(min_path, min_path_weight)
